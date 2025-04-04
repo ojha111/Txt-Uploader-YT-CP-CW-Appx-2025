6de20d9b999e299a7c3ba12d20cdfc9a078ed8ae
ojha111 <152144787+ojha111@users.noreply.github.com>
2025-04-04 05:33:10 +0530

    Update oldmain.py

--- oldmain.py
+++ oldmain.py
@@ -32,10 +32,10 @@ from base64 import b64encode, b64decode
 
 
 credit ="Caption:)™~" 
-OWNER = int(os.environ.get("OWNER", 7081036509))      #replace your user id 
+OWNER = int(os.environ.get("OWNER", 6573766001))      #replace your user id 
 # try: 
 #     ADMINS=[] 
-#     for x in (os.environ.get("ADMINS", "7081036509 7491167754").split()):     ##replace your user id  (for multiple userid use white space) e.g. "123456 987456" <<- two userid ka example haa yee 
+#     for x in (os.environ.get("ADMINS", "6573766001 7491167754").split()):     ##replace your user id  (for multiple userid use white space) e.g. "123456 987456" <<- two userid ka example haa yee 
 #         # jitni userid dalni ha usti daloo ...
 #         ADMINS.append(int(x)) 
 # except ValueError: 
